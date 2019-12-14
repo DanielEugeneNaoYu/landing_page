3939f304d692afe61261911b41760b41bf0c7e79
NaoArimura <naoyosetani@gmail.com>
2019-12-14 16:34:46 +0900

    success to show dummy video

---
+++ src/components/Videos.jsx
@@ -0,0 +1,33 @@
+
+import React, { Component } from 'react';
+import { Player, ControlBar, PlaybackRateMenuButton } from "video-react";
+
+
+class Videos extends Component {
+    componentDidMount() {
+      this.player.playbackRate = 1;
+      this.forceUpdate();
+    }
+    render() {
+      return (
+ 
+            <Player
+            ref={c => {
+                this.player = c;
+            }}
+            playsInline
+            >
+            <ControlBar>
+                <PlaybackRateMenuButton order={7} rates={[2, 1.5, 1.0, 0.5]} />
+            </ControlBar>
+            <source
+                src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                type="video/mp4"
+            />
+            </Player>
+        
+      );
+    }
+  }
+
+        export default Videos;
