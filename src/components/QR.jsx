7b94973a73bd795149baf2ead6162b3b5e5cbaa3
NaoArimura <naoyosetani@gmail.com>
2019-12-14 17:26:57 +0900

    wrote gist

---
+++ src/components/QR.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class QR extends React.Component{
+    render(){
+        return (
+            <div id='qrs'>
+                <h1>Available now</h1>
+
+            </div>
+        );
+    }
+
+}
+
+
+export default QR;
