e7cdeafca58d25261374f3eac0dba00b2b2cf962
NaoArimura <naoyosetani@gmail.com>
2019-12-15 12:49:33 +0900

    added anchor links from Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 class Navbar extends React.Component{
     render(){
         return(
             <div id="navbar">
-                <p className='inline-block-element'>I am Navbar</p>
-                <p className='inline-block-element'>Downloads</p>
-                <p className='inline-block-element'>Videos</p>
-                <p className='inline-block-element'>About</p>
+                <AnchorLink href="">
+                    <p className='inline-block-element'>I am Navbar</p>
+                </AnchorLink>
+                <AnchorLink href="#qrs">
+                    <p className='inline-block-element'>Downloads</p>
+                    </AnchorLink>
+                <AnchorLink href="#videos">
+                    <p className='inline-block-element'>Videos</p>
+                </AnchorLink>
+                <AnchorLink href="#about">
+                    <p className='inline-block-element'>About</p>
+                </AnchorLink>
             </div>
 
         );
