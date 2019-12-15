7c877ce0c3d17d10a9d470bbc0eaeadd86abb544
NaoArimura <naoyosetani@gmail.com>
2019-12-16 08:24:30 +0900

    modified height css in About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,27 +9,25 @@ class About extends React.Component {
                 <div id="title">
                     <h1>What is duelingo?</h1>
                 </div>
-                <div>
-                    <h2>Fighting Game!</h2>
-                </div>
-                <div>
-                    <p>explanation about game explanation about gameexplanation about gameexplanation about gameexplanation about gameexplanation about gameexplanation about gameexplanation about game</p>
-                </div>
+
+                <div id="aboutContainer">
+                    <div className="points">
+                        <h2>Fighting Game!</h2>
+                        <p>explanation about game explanation about gameexplanation about gameexplanation about gameexplanation about gameexplanation about gameexplanation about gameexplanation about game</p>
+                    </div>
 
 
-                <div>
-                    <h2>Review and Learning communitypage!</h2>
-                </div>
-                <div>
-                    <p>explanation about flashcards explanation about flashcardsexplanation about flashcardsexplanation about flashcardsexplanation about flashcardsexplanation about flashcardsexplanation about flashcardsexplanation about flashcards</p>
-                </div>
+                    <div className="points">
+                        <h2>Review and Learning communitypage!</h2>
+                        <p>explanation about flashcards explanation about flashcardsexplanation about flashcardsexplanation about flashcardsexplanation about flashcardsexplanation about flashcardsexplanation about flashcardsexplanation about flashcards</p>
+                    </div>
 
 
-                <div>
-                    <h2>Community Page</h2>
-                </div>
-                <div>
-                    <p>explanation about communitypage explanation about communitypageexplanation about communitypageexplanation about communitypageexplanation about communitypageexplanation about communitypageexplanation about communitypageexplanation about communitypage</p>
+                    <div className="points">
+                        <h2>Community Page</h2>
+                        <p>explanation about communitypage explanation about communitypageexplanation about communitypageexplanation about communitypageexplanation about communitypageexplanation about communitypageexplanation about communitypageexplanation about communitypage</p>
+                    </div>
+
                 </div>
             </div>
         );
