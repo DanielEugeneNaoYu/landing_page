8340c6b21f5774bb2c08732e6e7fc8afb4d02647
NaoArimura <naoyosetani@gmail.com>
2019-12-19 20:39:43 +0900

    embeded tweet timeline

--- src/components/Twitter.jsx
+++ src/components/Twitter.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 class Twitter extends React.Component {
+    render(){
+        return(
+                <div id="twitterContainer">
+                    <a class="twitter-timeline" href="https://twitter.com/home?ref_src=twsrc%5Etfw">Tweets by home</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+                </div>
+        );
+    }
 
 }
 
-export default Twitter;
+export default Twitter; 
