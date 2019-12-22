991101d6243eed85bcc5f79e8f62c28163553f15
NaoArimura <naoyosetani@gmail.com>
2019-12-22 11:50:15 +0900

    mofidied flashcard video area

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -10,80 +10,66 @@ class Videos extends React.Component {
     }
     render() {
       return (
-      
-      
-      <div>
-              <div id="videoTitle">
-                  <h1>What is LEXIVERSE ?</h1>
-              </div>
+        <div>
+            <div id="videoTitle">
+                <h1>What is LEXIVERSE ?</h1>
+            </div>
 
-          <Flexbox className="video" id="videos" flexDirection='column' alignItems='center'>
-          
-            <Flexbox className='video_containter' justifyContent='flex-end'>
-                    <Flexbox className='video_text' flexDirection="column">
-                        <h2>GAME</h2>
-                        <h4>interactive and fast paced game that will test gaming and language ability</h4>
+                <Flexbox className="video" id="videos" flexDirection='column' alignItems='center'>
+                
+                    <Flexbox className='video_containter'>
+                            <Flexbox className='video_text' flexDirection="column">
+                                <h2>GAME</h2>
+                                <h4>interactive and fast paced game that will test gaming and language ability</h4>
+                            </Flexbox>
+
+                            <Flexbox className='video_player'　id="game_video">
+                                    <Player
+                                    ref={c => {
+                                        this.player = c;
+                                    }}
+                                    playsInline
+                                    autoPlay='true'
+                                    >
+                                    <ControlBar>
+                                        <PlaybackRateMenuButton order={7} rates={[2, 1.5, 1.0, 0.5]} />
+                                    </ControlBar>
+                                    <source
+                                        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                                        type="video/mp4"
+                                        />
+                                    </Player>
+                            </Flexbox>
                     </Flexbox>
 
-                    <Flexbox className='video_player'　id="game_video">
-                            <Player
-                            ref={c => {
-                                this.player = c;
-                            }}
-                            playsInline
-                            autoPlay='true'
-                            >
-                            <ControlBar>
-                                <PlaybackRateMenuButton order={7} rates={[2, 1.5, 1.0, 0.5]} />
-                            </ControlBar>
-                            <source
-                                src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-                                type="video/mp4"
-                                />
-                            </Player>
+
+                    <Flexbox className='flashcard_container'>
+
+                            <Flexbox className='video_player'　id="flashcard_video">
+                                    <Player
+                                    ref={c => {
+                                        this.player = c;
+                                    }}
+                                    playsInline
+                                    autoPlay='true'
+                                    >
+                                    <ControlBar>
+                                        <PlaybackRateMenuButton order={7} rates={[2, 1.5, 1.0, 0.5]} />
+                                    </ControlBar>
+                                    <source
+                                        src="https://lexiverse-videos.s3-ap-northeast-1.amazonaws.com/demo.mp4"
+                                        type="video/mp4"
+                                        />
+                                    </Player>
+                            </Flexbox>
+                            <Flexbox className='flashcard_text' flexDirection="column">
+                                <h2>Flashcards</h2>
+                                <h4>accompanying flashcard app to supplement learning</h4>
+                            </Flexbox>
                     </Flexbox>
             </Flexbox>
-
-
-
-
-          
-         
-          
-          
-                
-       
-
-              <div id="flashcard_video">
-                    <Player
-                    ref={c => {
-                        this.player = c;
-                    }}
-                    playsInline
-                    autoPlay='true'
-                    >
-                    <ControlBar>
-                        <PlaybackRateMenuButton order={7} rates={[2, 1.5, 1.0, 0.5]} />
-                    </ControlBar>
-                    <source
-                        src="https://lexiverse-videos.s3-ap-northeast-1.amazonaws.com/demo.mp4"
-                        type="video/mp4"
-                        />
-                    </Player>
-              </div>
-                
-       
-                <br/>
-                <br/>
-                <br/>
-
-          </Flexbox>
-      </div>
-           
- 
-        
+        </div>
       );
     }
   }
-
-        export default Videos;
+export default Videos;
