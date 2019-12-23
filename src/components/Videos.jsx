9f44a57f98428ed1459aa1532e494e3064f72d00
NaoArimura <naoyosetani@gmail.com>
2019-12-23 09:34:13 +0900

    changed flashcard demo

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -35,7 +35,6 @@ class Videos extends React.Component {
                                         <PlaybackRateMenuButton order={7} rates={[2, 1.5, 1.0, 0.5]} />
                                     </ControlBar>
                                     <source
-                                        // src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
                                         src="https://lexiverse-videos.s3-ap-northeast-1.amazonaws.com/game.mp4"
                                         type="video/mp4"
                                         />
@@ -58,7 +57,8 @@ class Videos extends React.Component {
                                         <PlaybackRateMenuButton order={7} rates={[2, 1.5, 1.0, 0.5]} />
                                     </ControlBar>
                                     <source
-                                        src="https://lexiverse-videos.s3-ap-northeast-1.amazonaws.com/demo.mp4"
+                                        // src="https://lexiverse-videos.s3-ap-northeast-1.amazonaws.com/demo.mp4"
+                                        src="https://lexiverse-videos.s3-ap-northeast-1.amazonaws.com/flashcard_demo.mp4"
                                         type="video/mp4"
                                         />
                                     </Player>
