e1033fe35991db99ff84bd60974f3a696ff9f926
NaoArimura <naoyosetani@gmail.com>
2019-12-24 23:00:03 +0900

    added game qr

--- src/components/QR.jsx
+++ src/components/QR.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import qr1 from '../images/dummy.png'
+import qr1 from '../images/game_qr.png'
 import qr2 from '../images/googleplay_qr.png'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
 import Flexbox from 'flexbox-react';
 
 
@@ -10,11 +9,7 @@ class QR extends React.Component{
         return (
 
         <Flexbox id='qrs' flexDirection="column">
-
-       
                 <h1 id="qrtitile">Available now</h1>
-        
-
             <Flexbox justifyContent="center">
                 <div className='qrContainer'>
                     <h3>GAME</h3>    
@@ -26,24 +21,18 @@ class QR extends React.Component{
                         </a>
                     </div>
 
-                    <AnchorLink href="#game_video">
-                        <button className="btn　"><i className="fa fa-download"></i> Demo</button>
-                    </AnchorLink>
+                
                 </div>
 
                 <div className='qrContainer'>
                     <h3>FLASHCARDS</h3>    
                     <img className='qr' src={qr2} alt="flashcards"/>
               
-                    <div id="googleplay">
+                   
                     　　 <a href='https://play.google.com/store/apps/details?id=com.cc10.duelingoflashcard.duelingo_flashcard&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
                             <img alt='Get it on Google Play' id="googleplay_logo" src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/>
                         </a>
-                    </div>
-
-                    <AnchorLink href="#flashcard_video">
-                        <button className="btn　"><i className="fa fa-download"></i> Demo</button>
-                    </AnchorLink>
+                
                 </div>
 
             </Flexbox>
