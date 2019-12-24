d7a75d1a39ed270089ac6840dedf19b3128272c5
NaoArimura <naoyosetani@gmail.com>
2019-12-24 22:26:08 +0900

    test fro responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import {MediaQuery, SmartPhoneContents, PCContents} from "react-responsive";
 
 class Navbar extends React.Component{
     render(){
         return(
             <div id="navbar">
+
+            {/* <MediaQuery query="(max-width: 767px)">
+                <SmartPhoneContents />
+            </MediaQuery>
+            <MediaQuery query="(min-width: 767px)">
+                <PCContents />
+            </MediaQuery> */}
+
                 <AnchorLink href="#qrs">
                     <p className='inline-block-element'>Downloads</p>
                     </AnchorLink>
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -15,7 +15,62 @@ class Videos extends React.Component {
                 <h1>What is LEXIVERSE ?</h1>
             </div>
 
-                <Flexbox className="video" id="videos" flexDirection='column' alignItems='center'>
+
+            <div className="video vs" id="videos">
+                
+                <div className='video_containter vContainer'>
+                        <div className='video_text vt' flexDirection="column" justifyContent="center">
+                            <h2>GAME</h2>
+                            <h4>A interactive Japanese language learning game with multiple fast-paced game modes to keep the player engaged!</h4>
+                        </div>
+
+                        <div className='video_player vp'　id="game_video">
+                                <Player
+                                ref={c => {
+                                    this.player = c;
+                                }}
+                                playsInline
+                                autoPlay='true'
+                                >
+                                <ControlBar>
+                                    <PlaybackRateMenuButton order={7} rates={[2, 1.5, 1.0, 0.5]} />
+                                </ControlBar>
+                                <source
+                                    src="https://lexiverse-videos.s3-ap-northeast-1.amazonaws.com/game.mp4"
+                                    type="video/mp4"
+                                    />
+                                </Player>
+                        </div>
+                </div>
+
+
+                <div className='flashcard_container'>
+
+                        <div className='video_player'　id="flashcard_video">
+                                <Player
+                                ref={c => {
+                                    this.player = c;
+                                }}
+                                playsInline
+                                // autoPlay='true'
+                                >
+                                <ControlBar>
+                                    <PlaybackRateMenuButton order={7} rates={[2, 1.5, 1.0, 0.5]} />
+                                </ControlBar>
+                                <source
+                                    src="https://lexiverse-videos.s3-ap-northeast-1.amazonaws.com/flashcard_demo.mp4"
+                                    type="video/mp4"
+                                    />
+                                </Player>
+                        </div>
+                        <div className='flashcard_text' flexDirection="column">
+                            <h2>Flashcards</h2>
+                            <h4>Don't forget to check out the accompaying flashcard app to supplement the learning material! Comes with various types of queries, allowing the user to quickly narrow into their weak points!</h4>
+                        </div>
+                </div>
+        </div>
+
+                {/* <Flexbox className="video" id="videos" flexDirection='column' alignItems='center'>
                 
                     <Flexbox className='video_containter'>
                             <Flexbox className='video_text' flexDirection="column" justifyContent="center">
@@ -67,7 +122,7 @@ class Videos extends React.Component {
                                 <h4>Don't forget to check out the accompaying flashcard app to supplement the learning material! Comes with various types of queries, allowing the user to quickly narrow into their weak points!</h4>
                             </Flexbox>
                     </Flexbox>
-            </Flexbox>
+            </Flexbox> */}
         </div>
       );
     }
